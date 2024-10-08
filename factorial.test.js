ea1ad9f390865afb1abd475dfc87c2bf125673ba
Jam <jam29n@gmail.com>
2024-10-08 09:14:54 +0200

    ajout test fact(3)

--- factorial.test.js
+++ factorial.test.js
@@ -9,6 +9,10 @@ test('factorial of 4 should be 24', () => {
   expect(factorial(4)).toBe(24);
 });
 
+test('factorial of 3 should be 6', () => {
+    expect(factorial(3)).toBe(6);
+  });
+
 test('factorial of 0 should be 1', () => {
     expect(factorial(0)).toBe(1);
 });
@@ -16,4 +20,6 @@ test('factorial of 0 should be 1', () => {
 test('factorial of negative numbers should be -1', () => {
     expect(factorial(-5)).toBe(-1);
 });
+
+
 
